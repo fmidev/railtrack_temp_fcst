237c5fe04664ceb07abf4f381818113229c691b0
KorpinenA <anniina.korpinen@fmi.fi>
2024-01-11 15:44:58 +0200

    Change metadata sourcefile, mask missing data if any

--- generate_ML_temperature_rail_fcst.py
+++ generate_ML_temperature_rail_fcst.py
@@ -13,18 +13,22 @@ def main():
     args = parse_command_line()
     forecast_params = tl.fetch_basic_predictor_files(args)
     for i, (param_name, param_file) in enumerate(forecast_params.items()):
-        if param_name == 'T2':
+        if param_name == 'SKT':
             data_meta = ReadData(param_file, use_as_template=True, read_coordinates=True)
         if i == 0:
             data = ReadData(param_file, read_coordinates=True, time_steps=125)
+            data = tl.mask_missing_data(data)
             ds = tl.create_dataset(data, param_name)
             ds = tl.calculate_forecast_period_dataset(data.dtime, ds)
             continue
         if param_name == 'SRR1h' or param_name == 'STR1h':
             data_rad = ReadData(param_file, time_steps=125, missing_data=True)
+            data_rad = tl.mask_missing_data(data_rad)
+            #TODO: tarkasta toimiiko maskin kanssa
             ds = tl.calculate_hourly_values_dataset(data_rad.data, param_name, ds)
             continue
         data = ReadData(param_file, time_steps=125)
+        data = tl.mask_missing_data(data)
         ds = tl.add_data_to_dataset(data, param_name, ds)
     ds = tl.calculate_angle_time_dataset(data.dtime, ds)
     ds = tl.convert_percentage_to_zero_one(['LCC', 'MCC'], ds)
